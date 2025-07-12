bed60e17993cb5b2ed1a2651b17216f219c6d603
Aarogya <aarufloyd@gmail.com>
2025-07-12 16:29:46 +0545

    This is a counter program

---
+++ index.js
@@ -0,0 +1,38 @@
+const increase=document.getElementById("increase");
+const reset=document.getElementById("reset");
+ const decrease=document.getElementById("decrease");
+ const Clabel=document.getElementById("clabel");
+let c=0;
+
+increase.onclick=function()
+{ if (c < 10) {
+    c++;
+    Clabel.textContent = c;
+    if (c === 10) {
+        alert(`YOU are at your maximum limit of ${c}`);
+    }
+} else {
+    alert("Sorry, can't go above 10");
+}
+
+}
+
+    
+decrease.onclick=function()
+{
+if(c>0){
+        c--;
+    Clabel.textContent=c;
+    }
+   else
+   {
+    window.alert("Soory can't go below 0");
+    
+   }
+}
+reset.onclick=function()
+{
+    c=0;
+    Clabel.textContent=c;
+
+}
